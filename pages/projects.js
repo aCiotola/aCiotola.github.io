fdca3b4e8b142c624931f4f2c4cf213120881cc2
Alessandro Ciotola <alessandromciotola@gmail.com>
2022-05-04 15:58:30 -0400

    Added Java Project.
    Added more projects

--- pages/projects.js
+++ pages/projects.js
@@ -1,9 +1,20 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
+$(document).ready(function () {
+  $('.sidenav:first').addClass('active');
+  $('.tab-content:not(:first)').hide();
+  $('.sidenav a').click(function (event) {
+      event.preventDefault();
+      var content = $(this).attr('href');
+      $(this).parent().addClass('active');
+      $(this).parent().siblings().removeClass('active');
+      $(content).show();
+      $(content).siblings('.tab-content').hide();
+  });
+});
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.documentElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
